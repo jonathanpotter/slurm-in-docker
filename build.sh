bcad11992de1bcb2e5c00a04c4babf29d3efb7fe
Jonathan Potter <jpotte46@ford.com>
2019-12-05 21:18:32 -0500

    Bring up workers

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 IMAGE_TAG=0.0.1
 #SUBDIR_LIST='base controller worker database'
-SUBDIR_LIST='base controller'
+SUBDIR_LIST='base controller worker'
 
 for SUBDIR in ${SUBDIR_LIST}; do
   sudo podman build -f ${SUBDIR}/Dockerfile -t ${SUBDIR}:${IMAGE_TAG} ./${SUBDIR}
