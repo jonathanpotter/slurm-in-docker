c40ffabd7b408992a2fbeb6a585e048b818d24dd
Jonathan Potter <jpotte46@ford.com>
2019-12-05 20:25:04 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Builds the container images for cluster.
+
+IMAGE_TAG=0.0.1
+#SUBDIR_LIST='base controller worker database'
+SUBDIR_LIST='base'
+
+for SUBDIR in ${SUBDIR_LIST}; do
+  sudo podman build -f ${SUBDIR}/Dockerfile -t ${SUBDIR}:${IMAGE_TAG} ./${SUBDIR}
+done
